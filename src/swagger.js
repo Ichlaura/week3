0988e69164eee8912b8daa4f8913a11fcff9603c
laura nunez <laupao41@gmail.com>
2025-10-09 16:55:54 +0900

    Fix Swagger host and output for Render

--- src/swagger.js
+++ src/swagger.js
@@ -9,10 +9,14 @@ const doc = {
     description: 'API for items and orders CRUD operations with authentication',
     version: '1.0.0'
   },
-  host: process.env.NODE_ENV === 'production' 
-    ? 'week3-i0vy.onrender.com' 
-    : 'localhost:8080',
-  schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http'],
+
+    host: 'week3-i0vy.onrender.com',
+  schemes: ['https'],
+  //host: process.env.NODE_ENV === 'production' 
+   // ? 'week3-i0vy.onrender.com' 
+    //: 'localhost:8080',
+  //schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http'],
+  
   securityDefinitions: {
     bearerAuth: {
       type: 'apiKey',
